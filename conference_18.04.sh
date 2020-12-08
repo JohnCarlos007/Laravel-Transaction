d9a88109618778edf47071cfd83ad57c1968ebe9
JohnCarlos007 <64250077+JohnCarlos007@users.noreply.github.com>
2020-12-08 16:26:27 +0800

    Clarification for commands without -y

--- conference_18.04.sh
+++ conference_18.04.sh
@@ -12,9 +12,9 @@ sudo apt-get -y install software-properties-common
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt -y install php7.4
-sudo apt install php7.4-common php7.4-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-imap php7.4-xml php7.4-zip
+sudo apt install -y php7.4-common php7.4-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-imap php7.4-xml php7.4-zip
 sudo systemctl restart apache2
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
